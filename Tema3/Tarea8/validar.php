e9a0f7b8e0b3cef3a21ac82be3bca78bace8836d
guzman <guzmanghu@gmail.com>
2022-10-28 12:20:34 +0000

    ult

--- Tema3/Tarea8/validar.php
+++ Tema3/Tarea8/validar.php
@@ -1,53 +1,72 @@
 <?php
 
-    //funcion para saber si el texto esta vacio
-    function vacio($nombre){
-        if (empty($_REQUEST[$nombre])) {
-            return true;
-        }else{
-            return false;
-        }
-    }
-    //funcion para comprobar que se ha pulsado l boton enviar
-    function enviado(){
-        if (isset($_REQUEST['enviar'])) {
-            return true;
-        }else {
-            return false;
-        }
-    }
-    //funcion para comprobar que hay una opcion seleccionada
-    function existe($nombre){
-        if (isset($_REQUEST[$nombre])) {
-            return true;
-        }else {
-            return false;
-        }
-    }
-    function controlChecks($nombre){
-        $conta=0;
-        if (isset($_REQUEST[$nombre])) {
-            $conta = count($_REQUEST[$nombre]);
-        }
-        if ($conta <1 || $conta>3) {
-            return false;
-        }else {
-            return true;
-        }
-    }
-
-    function alfabetico($nombre) {
-        $conta=0;
-        $nombre = array();
-        foreach ($nombre as $key => $value) {
-            if(is_int($value)){
-                $conta++;
+        //funcion para saber si el texto esta vacio
+        function vacio($nombre){
+            if (empty($_REQUEST[$nombre])) {
+                return true;
+            }else{
+                return false;
             }
         }
-        if($conta>0){
-            return false;
-        }else{
-            return true;
+        //funcion para comprobar que se ha pulsado l boton enviar
+        function enviado(){
+            if (isset($_REQUEST['enviar'])) {
+                return true;
+            }else {
+                return false;
+            }
         }
+
+        //funcion para comprobar que hay una opcion seleccionada
+        function existe($nombre){
+            if (isset($_REQUEST[$nombre])) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+        
+        function controlChecks($nombre){
+            $conta=0;
+            if (isset($_REQUEST[$nombre])) {
+                $conta = count($_REQUEST[$nombre]);
+            }
+            if ($conta <1 || $conta>3) {
+                return false;
+            }else {
+                return true;
+            }
+        }
+
+    function validarTodo(){
+        if(enviado()){
+            if ( !vacio($_REQUEST["nombre"]) && !vacio($_REQUEST["apellido1"]) && !vacio($_REQUEST["fecha"]) && !existe($_REQUEST["opcion"]) ) {
+               return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+ 
     }
+
+    function mostraResultados(){
+        echo "<h1>Datos introducidos</h1>";
+        echo "Alfabetico: ".$_REQUEST["nombre"];
+        if (!vacio("nombre2")) {
+            echo "Alfabetico Opcional: ".$_REQUEST["nombre2"];
+        }
+        echo "Alfanumerico: ".$_REQUEST["apellido1"];
+        if (!vacio("apellido2")) {
+            echo "Alfanumerico Opcional: ".$_REQUEST["apellido2"];
+        }
+
+    }
+
+
+
+
+
+
 ?>
