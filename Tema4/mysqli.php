f9a45184d94753e74f2d3262c42735a62d186e71
guzman <guzmanghu@gmail.com>
2022-12-01 12:10:37 +0000

    CLASE

--- Tema4/mysqli.php
+++ Tema4/mysqli.php
@@ -2,12 +2,15 @@
 
 require './seguro/conexion.php';
 try{
-    $conexion = mysqli_connect(HOST,'USER',PASS);
+    $conexion = mysqli_connect('HOST',USER,PASS);
 }catch(Exception $ex){
-    echo $ex;
+    if ($ex->getCode() == 2002) {
+        echo "Error, no están bien configurada la base de datos en el wp-config. php";
+    }
+    if ($ex->getCode() == 1045) {
+        echo "Error, Puede deberse a haber cambiado la password al usuario root@localhost en privilegios de usuarios.";
+    }
 }
-
-
 echo "";
 
 ?>
